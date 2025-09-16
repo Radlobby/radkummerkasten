dc1784c1622a43dbbcede8e116a2151aa89591d6
Christoph Fink <christoph.fink@helsinki.fi>
2025-09-16 22:56:04 +0200

    comment unused code out for now

--- src/radkummerkasten/utilities/decorators/__init__.py
+++ src/radkummerkasten/utilities/decorators/__init__.py
@@ -5,9 +5,9 @@
 
 
 from .local_referer_only import local_referer_only
-from .login_required import login_required
+# from .login_required import login_required
 
 __all__ = [
     "local_referer_only",
-    "login_required",
+    # "login_required",
 ]
